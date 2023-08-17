22fb760121a23072c180261f9a3133c2e78101a5
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2023-08-17 11:39:15 +0600

    setup added

---
+++ app/(landing)/page.jsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+    <>
+      <p className="">
+        Hello Next 13
+      </p>
+
+      <p className="typography__subhead">
+        Hello Next 13 Green
+      </p>
+    </>
+  )
+}
---
+++ next.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = {
+    sassOptions: {
+        includePaths: [path.join(__dirname, './app/styles')],
+    },
+}
