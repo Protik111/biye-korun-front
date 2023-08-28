8f76ec28064cc0ce691f748aee9ac64bfe1f1e10
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2023-08-28 15:38:21 +0600

    navbar added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,21 @@
-import './globals.scss'
-import { Inter } from 'next/font/google'
+import Navbar from "@/components/global/Navbar";
+import "./globals.scss";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Biye Korun',
-  description: 'A matrimony service to find people hapiness',
-}
+  title: "Biye Korun",
+  description: "A matrimony service to find people hapiness",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar></Navbar>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
