6e09a81ccfa6195e8d0e61e34c38027f068f753d
mdshimul186 <62536223+mdshimul186@users.noreply.github.com>
2023-09-05 13:39:54 +0600

    update

--- app/(profile-creation)/layout.jsx
+++ app/(profile-creation)/layout.jsx
@@ -1,39 +1,16 @@
-"use client"
-import { loadUser } from "@/redux/features/auth/authSlice";
-import { Loader } from "@mantine/core";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux"
+// "use client"
+// import { loadUser } from "@/redux/features/auth/authSlice";
+// import { Loader } from "@mantine/core";
+// import { useRouter } from "next/navigation";
+// import { useEffect, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux"
 
-// export const metadata = {
-//   title: 'Profile Creation',
-//   description: 'A matrimony service to find people hapiness',
-// }
+export const metadata = {
+  title: 'Profile Creation',
+  description: 'A matrimony service to find people hapiness',
+}
 
 export default function RootLayout({ children }) {
-  const { isAuthenticated, user } = useSelector(state => state.auth);
-  const dispatch = useDispatch();
-  const [isLoading, setIsLoading] = useState(false);
-
-  const router = useRouter();
-
-  // useEffect(() => {
-  //   const token = localStorage.getItem('biyeKorun_token')
-  //   if (!token) {
-  //     console.log('trruu');
-  //     router.push('/');
-  //     // return
-  //   }
-
-  //   setIsLoading(true)
-
-  // }, [router])
-
-  // if (!isLoading) {
-  //   return <div className="flex justify-center align-center min-vh-100">
-  //     <Loader size="xl" color="pink" />
-  //   </div>
-  // }
 
   return (
     <div>
