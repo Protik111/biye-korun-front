9733869931d88b9b93d67964f51d175350666e3c
mdshimul186 <62536223+mdshimul186@users.noreply.github.com>
2023-09-05 13:31:54 +0600

    cicd added

--- app/(profile-creation)/layout.jsx
+++ app/(profile-creation)/layout.jsx
@@ -1,14 +1,14 @@
-
+"use client"
 import { loadUser } from "@/redux/features/auth/authSlice";
 import { Loader } from "@mantine/core";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux"
 
-export const metadata = {
-  title: 'Profile Creation',
-  description: 'A matrimony service to find people hapiness',
-}
+// export const metadata = {
+//   title: 'Profile Creation',
+//   description: 'A matrimony service to find people hapiness',
+// }
 
 export default function RootLayout({ children }) {
   const { isAuthenticated, user } = useSelector(state => state.auth);
