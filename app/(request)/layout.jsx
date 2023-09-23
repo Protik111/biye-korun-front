fcf602d79e1cf216e55e9aedf6d2794ed4b5c2b4
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2023-09-23 16:40:06 +0600

    [DONE] sent req and filter added

--- app/(request)/layout.jsx
+++ app/(request)/layout.jsx
@@ -3,10 +3,6 @@
 import "../globals.scss";
 import RequestTapbar from "@/components/request/RequestTapbar";
 
-export const metadata = {
-  title: "Matches",
-  description: "A matrimony service to find people hapiness",
-};
 
 export default function RootLayout({ children }) {
   return (
