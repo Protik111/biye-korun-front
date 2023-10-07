77e746f7435a2d430b29fbd95c8584bfb98b2a33
Julfikar-Haidar <julfikar6262@gmail.com>
2023-10-07 12:18:59 +0600

    date field change

--- components/landing/LandingV2.jsx
+++ components/landing/LandingV2.jsx
@@ -1,7 +1,13 @@
-'use client';
+"use client";
 
-import { btnBackground, labelStyles, selectMobileStyles, selectMobileStylesV2 } from "@/styles/library/mantine";
-import { Button, Select } from "@mantine/core";
+import {
+  btnBackground,
+  labelStyles,
+  selectMobileStyles,
+  selectMobileStylesV2,
+  selectMultiStyles,
+} from "@/styles/library/mantine";
+import { Button, Select, MultiSelect } from "@mantine/core";
 import { useState } from "react";
 import CenteredModal from "../global/CenteredModal";
 import MultistepRegistration from "../multiStepRegistration/multistepRegistration";
@@ -14,66 +20,82 @@ import { useSelector } from "react-redux";
 import { useRouter } from "next/navigation";
 
 const LandingV2 = () => {
-    const { isAuthenticated } = useSelector(state => state.auth)
-    const router = useRouter();
+  const { isAuthenticated } = useSelector((state) => state.auth);
+  const router = useRouter();
 
-    const [modalOpen, setModalOpen] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
 
-    // console.log('matches', matches);
+  // console.log('matches', matches);
 
-    const startAge = 20;
-    const endAge = 70;
-    const ages = Array.from({ length: endAge - startAge + 1 }, (_, index) => (startAge + index).toString().padStart(2, '0'));
+  const startAge = 20;
+  const endAge = 70;
+  const ages = Array.from({ length: endAge - startAge + 1 }, (_, index) =>
+    (startAge + index).toString().padStart(2, "0")
+  );
 
-    const handleModalClose = () => setModalOpen(false)
+  const handleModalClose = () => setModalOpen(false);
 
-    const handleLetsBegin = () => {
-        if (!isAuthenticated) {
-            setModalOpen(true)
-        } else {
-            router.push('/my-matches')
-        }
+  const handleLetsBegin = () => {
+    if (!isAuthenticated) {
+      setModalOpen(true);
+    } else {
+      router.push("/my-matches");
     }
+  };
 
-    return (
-        <div className='landingv2'>
-            <div className="slider">
-                <div className="slide slide1"></div>
-                <div className="slide slide2"></div>
-                <div className="slide slide3"></div>
-                <div className="slide slide4"></div>
-                <div className="slide slide5"></div>
-                <div className="slide slide6"></div>
-            </div>
+  return (
+    <div className="landingv2">
+      <div className="slider">
+        <div className="slide slide1"></div>
+        <div className="slide slide2"></div>
+        <div className="slide slide3"></div>
+        <div className="slide slide4"></div>
+        <div className="slide slide5"></div>
+        <div className="slide slide6"></div>
+      </div>
 
+      <div className="container">
+        <div className="landing__meet">
+          <div className="landing__meet--form p-30 rounded-10">
+            <h2 className="landing__meet--title text-center">
+              Find the right person to spend rest of your life
+            </h2>
 
-            <div className="container">
-                <div className="landing__meet">
-                    <div className="landing__meet--form p-30 rounded-10">
-                        <h2 className="landing__meet--title text-center">Find the right person to spend rest of your life</h2>
+            <div className="flex justify-center align-center flex-gap-10 flex-wrap mt-15">
+              <Select
+                size="md"
+                label="Looking For"
+                styles={{ label: labelStyles }}
+                defaultValue="woman"
+                data={[
+                  { value: "woman", label: "Bride" },
+                  { value: "Men", label: "Groom" },
+                ]}
+                sx={selectMobileStyles}
+              />
 
-                        <div className="flex justify-center align-center flex-gap-25 flex-wrap mt-15">
-                            <Select
-                                size="md"
-                                label="Looking For"
-                                styles={{ label: labelStyles }}
-                                defaultValue="woman"
-                                data={[
-                                    { value: 'woman', label: 'Woman' },
-                                    { value: 'men', label: 'Men' },
-                                ]}
-                                sx={selectMobileStyles}
-                            />
+              <Select
+                size="md"
+                label="Age"
+                defaultValue="20"
+                styles={{ label: labelStyles }}
+                data={ages}
+                sx={selectMobileStylesV2}
+                min={20}
+              />
 
-                            <Select
-                                size="md"
-                                label="Age"
-                                defaultValue="20"
-                                styles={{ label: labelStyles }}
-                                data={ages}
-                                sx={selectMobileStylesV2}
-                            />
-                            {/* <p className="landing__meet--to">to</p>
+              <p style={{ marginTop: "21px", color: "white" }}>to</p>
+              <Select
+                size="md"
+                label=""
+                defaultValue="25"
+                styles={{ label: labelStyles }}
+                data={ages}
+                sx={selectMobileStylesV2}
+                max={70}
+                style={{ marginTop: "21px", color: "white" }}
+              />
+              {/* <p className="landing__meet--to">to</p>
 
                             <Select
                                 size="md"
@@ -86,55 +108,63 @@ const LandingV2 = () => {
 
                             /> */}
 
-                            <Select
-                                size="md"
-                                placeholder="Select"
-                                label="Religion"
-                                defaultValue="20"
-                                styles={{ label: labelStyles }}
-                                data={religions}
-                                sx={selectMobileStyles}
+              <Select
+                size="md"
+                placeholder="Select"
+                label="Religion"
+                defaultValue="20"
+                styles={{ label: labelStyles }}
+                data={religions}
+                sx={selectMobileStyles}
+              />
 
-                            />
+              <MultiSelect
+                size="md"
+                placeholder="Select"
+                label="Language"
+                defaultValue="20"
+                styles={{ label: labelStyles }}
+                data={motherTongues}
+                // style={{ height: "40px", overflow: "auto" }}
+                sx={selectMultiStyles}
+              />
 
-                            <Select
-                                size="md"
-                                placeholder="Select"
-                                label="Language"
-                                defaultValue="20"
-                                styles={{ label: labelStyles }}
-                                data={motherTongues}
-                                // style={{ width: '180px' }}
-                                sx={selectMobileStyles}
+              <div>
+                <Button
+                  className="mt-25"
+                  style={btnBackground}
+                  size="md"
+                  onClick={handleLetsBegin}
+                >
+                  Let's start...
+                </Button>
+              </div>
+            </div>
+          </div>
 
-                            />
-
-                            <div>
-                                <Button className="mt-25" style={btnBackground} size="md" onClick={handleLetsBegin}>
-                                    Let's start...
-                                </Button>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* <div className="flex justify-center mt-15">
+          {/* <div className="flex justify-center mt-15">
                         <Button style={btnBackground} size="md" onClick={handleLetsBegin}>
                             Let's Begin
                         </Button>
                     </div> */}
-                </div>
-            </div>
-
-            {
-                modalOpen && <CenteredModal modalOpen={modalOpen} handleModalClose={handleModalClose} modalTitle={<h3 className="text-center">Let's create an account!</h3>}>
-                    {/* <MultistepRegistration></MultistepRegistration> */}
-                    <FormProvider>
-                        <Form handleModalClose={handleModalClose}></Form>
-                    </FormProvider>
-                </CenteredModal>
-            }
         </div>
-    )
-}
+      </div>
 
-export default LandingV2
+      {modalOpen && (
+        <CenteredModal
+          modalOpen={modalOpen}
+          size="md"
+          handleModalClose={handleModalClose}
+          modalTitle={<h3 className="text-center">Let's create an account!</h3>}
+        >
+          {/* <MultistepRegistration></MultistepRegistration> */}
+          <FormProvider>
+            <Form handleModalClose={handleModalClose}></Form>
+          </FormProvider>
+        </CenteredModal>
+      )}
+    </div>
+  );
+};
+
+export default LandingV2;
