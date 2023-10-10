640d221d55b6d14b1431b1768f923af4a5bc2a60
Julfikar-Haidar <julfikar6262@gmail.com>
2023-10-10 15:19:09 +0600

    update

--- staticData/image.js
+++ staticData/image.js
@@ -1,3 +1,4 @@
-export const imageUrl = 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=250&q=80';
+export const imageUrl =
+  "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=250&q=80";
 
-export const notSpecfied = "Not Specified";
+export const notSpecfied = "Not Specfied";
