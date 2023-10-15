b91daf9e44527896ccad4a4cf770e60717a404a2
Shahadat Hossain <projects.techserve4u@techserve4u.com>
2023-10-15 16:31:00 +0600

    payment method integrate createOrder, onApprove, and forceRender integration done

--- app/plans/payment/[packageId]/page.jsx
+++ app/plans/payment/[packageId]/page.jsx
@@ -1,11 +1,21 @@
 "use client"
 import PaymentHero from "@/components/plans/payment/PaymentHero"
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 const page = () => {
+    const initialOptions = {
+        clientId: "AQ__BQbYyOTxhq16IctWBVA8bU-f7rkUHnEGLf8GOdHcy12o-BcjmV2vDBn_I15Sdr5reTDhXmQ-H01w",
+        currency: "USD",
+        intent: "capture",
+    };
+
     return (
-        <div className='plans'>
-            <PaymentHero></PaymentHero>
-        </div>
+        <PayPalScriptProvider options={initialOptions}>
+            <div className='plans'>
+                <PaymentHero></PaymentHero>
+            </div>
+        </PayPalScriptProvider>
+
     )
 }
 
