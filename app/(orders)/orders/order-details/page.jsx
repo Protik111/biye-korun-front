8fa4007835022be74bcea1d24a78270a82343279
ayman-awal <ayman.awal36@gmail.com>
2023-10-22 22:04:50 -0400

    order pages

---
+++ app/(orders)/orders/order-details/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+import OrderPage from "@/components/orders/order-details/OrderPage";
+
+const OrderDetails = () => {
+    return (
+        <>
+            <OrderPage></OrderPage>
+        </>
+    );
+};
+
+export default OrderDetails;
