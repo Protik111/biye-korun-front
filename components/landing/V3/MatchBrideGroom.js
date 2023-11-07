7daa4036a7193753e2c460aecbcb5d96d8682ac8
Julfikar-Haidar <julfikar6262@gmail.com>
2023-11-07 12:47:57 +0600

    update

--- components/landing/V3/MatchBrideGroom.js
+++ components/landing/V3/MatchBrideGroom.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@mantine/core";
 import { BiDotsHorizontalRounded } from "react-icons/bi";
 import { BsArrowRight } from "react-icons/bs";
-let data = [
-  { id: "1" },
-  { id: "2" },
-  { id: "3" },
-  { id: "4" },
-  { id: "5" },
-  { id: "6" },
-  { id: "7" },
-  { id: "8" },
-];
+import { useSelector } from "react-redux";
+import useAxios from "@/hooks/axios/useAxios";
+import { useRouter } from "next/navigation";
+import { imageUrl } from "@/staticData/image";
+import { abbreviateName } from "@/utils/abbreviation";
+import { calculateAge } from "@/utils/calculateAge";
+import { heightCalculator } from "@/utils/heightCalculator";
+import CenteredModal from "@/components/global/CenteredModal";
+import { FormProvider } from "@/context/FormContext";
+import Form from "@/components/multiStepRegistration/Form";
+
 export const MatchBrideGroom = () => {
+  const { isAuthenticated } = useSelector((state) => state.auth);
+  const { data, error, loading, refetch } = useAxios("user/public-profile");
+  const router = useRouter();
+
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleModalClose = () => setModalOpen(false);
+
+  const handleRegister = () => {
+    if (!isAuthenticated) {
+      setModalOpen(true);
+    } else {
+      router.push("/my-matches");
+    }
+  };
+
+  const handleRedirect = () => {
+    router.push("/login");
+  };
+
   return (
     <div className="matches_main mb-30">
       <div className="container">
@@ -26,47 +47,94 @@ export const MatchBrideGroom = () => {
             </Button>
           </div>
         </div>
-        <div class="card-container">
-          {data?.map((item) => {
-            return (
-              <div className="card">
-                <div className="header">
-                  <div className="flex flex-gap-10 justify-between align-center">
-                    <div className="flex flex-gap-10 align-center">
-                      <h3 className="name heading3V3"> AR </h3>
-                      <span>#54326</span>
+        {data?.data?.length > 0 ? (
+          <div class="card-container">
+            {data?.data?.map((item, i) => {
+              return (
+                <div className="card">
+                  <div className="header">
+                    <div className="flex flex-gap-10 justify-between align-center">
+                      <div className="flex flex-gap-10 align-center">
+                        <h3 className="name heading3V3">
+                          {" "}
+                          {abbreviateName(
+                            item?.firstName + " " + item?.lastName
+                          )}
+                        </h3>
+                        <span>#{item.userId}</span>
+                      </div>
+                      <div>
+                        <BiDotsHorizontalRounded />
+                      </div>
                     </div>
-                    <div>
-                      <BiDotsHorizontalRounded />
+                    <p className="subtitle paragraphV3">
+                      {item?.educationCareer?.occupation
+                        ? item?.educationCareer?.occupation
+                        : "Software Engineer"}
+                    </p>
+                  </div>
+                  <div className="content">
+                    <div className="details">
+                      <p className="paragraphV3">
+                        {" "}
+                        {item?.dateOfBirth
+                          ? `${calculateAge(item?.dateOfBirth)} years`
+                          : "18 years"}
+                        ,{" "}
+                        {heightCalculator(item?.basicInfo?.height) ||
+                          `5 feet 10 inches`}
+                      </p>
+                      <p className="paragraphV3">
+                        {item?.location.city}, {item?.location.country}
+                      </p>
                     </div>
                   </div>
-                  <p className="subtitle paragraphV3">Software Engineer - Graduate</p>
-                </div>
-                <div className="content">
-                  <div className="details">
-                    <p className="paragraphV3">25 years, 5ft 5 in</p>
-                    <p className="paragraphV3">Islam Dhaka, Bangladesh</p>
+                  <div className="footer">
+                    <button className="custom-button" onClick={handleRedirect}>
+                      Send Request
+                    </button>
+                    <button
+                      className="custom-button btn_clr"
+                      onClick={handleRedirect}
+                    >
+                      View Profile
+                    </button>
+                  </div>
+                  <div class="avatar">
+                    <img
+                      src={item?.profilePicture?.url?.medium || imageUrl}
+                      alt={item?.name}
+                    />
                   </div>
                 </div>
-                <div className="footer">
-                  <button className="custom-button">Send Request</button>
-                  <button className="custom-button btn_clr">
-                    View Profile
-                  </button>
-                </div>
-                <div class="avatar">
-                  <img src="/landing/shiblu.png" alt="Avatar" />
-                </div>
-              </div>
-            );
-          })}
-        </div>
+              );
+            })}
+          </div>
+        ) : (
+          <>No data found</>
+        )}
         <div className="register_btn flex justify-center">
-          <button className="custom-button reg_btn_clr flex justify-center align-center flex-gap-5">
-            Register Now <BsArrowRight></BsArrowRight>
+          <button
+            className="custom-button reg_btn_clr flex justify-center align-center flex-gap-5"
+            onClick={handleRegister}
+          >
+            {!isAuthenticated ? "Register Now" : "Let's Begin"}{" "}
+            <BsArrowRight></BsArrowRight>
           </button>
         </div>
       </div>
+      {modalOpen && (
+        <CenteredModal
+          modalOpen={modalOpen}
+          handleModalClose={handleModalClose}
+          modalTitle={<h3 className="text-center">Let's Create an Account!</h3>}
+        >
+          {/* <MultistepRegistration></MultistepRegistration> */}
+          <FormProvider>
+            <Form handleModalClose={handleModalClose}></Form>
+          </FormProvider>
+        </CenteredModal>
+      )}
     </div>
   );
 };
