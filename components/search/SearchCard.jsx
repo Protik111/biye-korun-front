380fc4c1db0f6872b26a824a80d2d5aeeef934a8
Julfikar-Haidar <julfikar6262@gmail.com>
2023-11-27 15:41:47 +0600

    issue resolve browser

--- components/search/SearchCard.jsx
+++ components/search/SearchCard.jsx
@@ -15,9 +15,9 @@ import Form from "@/components/multiStepRegistration/Form";
 import { useEffect } from "react";
 import useAnimation from "@/hooks/common/useAnimation";
 
-export const SearchCard = () => {
+export const SearchCard = ({ profile, loading: loadingPrev, refetch }) => {
   const { isAuthenticated } = useSelector((state) => state.auth);
-  const { data, error, loading, refetch } = useAxios("user/public-profile");
+  // const { data, error, loading, refetch } = useAxios("user/public-profile");
   const router = useRouter();
 
   const [modalOpen, setModalOpen] = useState(false);
@@ -40,79 +40,80 @@ export const SearchCard = () => {
 
   return (
     <div>
-      {data?.data?.length > 0 ? (
-        <div className="card-container">
-          {data?.data?.map((item, i) => {
-            return (
-              <div className="card">
-                <div className="header">
-                  <div className="flex flex-gap-10 justify-between align-center">
-                    <div className="flex flex-gap-10 align-center">
-                      <h3 className="name heading3V3">
-                        {" "}
-                        {abbreviateName(item?.firstName + " " + item?.lastName)}
-                      </h3>
-                      <p className="hide_mobile">#{item.userId}</p>
-                    </div>
-                    <div>
-                      <BiDotsHorizontalRounded />
-                    </div>
-                  </div>
-                  {/* <div>
-                    <p>#{item.userId}</p>
+      {/* {data?.data?.length > 0 ? ( */}
+      <div className="card-container">
+        {/* {data?.data?.map((profile, i) => {
+            return ( */}
+        <div className="card">
+          <div className="header">
+            <div className="flex flex-gap-10 justify-between align-center">
+              <div className="flex flex-gap-10 align-center">
+                <h3 className="name heading3V3">
+                  {" "}
+                  {abbreviateName(profile?.firstName + " " + profile?.lastName)}
+                </h3>
+                <p className="hide_mobile">#{profile?.userId}</p>
+              </div>
+              <div>
+                <BiDotsHorizontalRounded />
+              </div>
+            </div>
+            {/* <div>
+                    <p>#{profile.userId}</p>
                   </div> */}
-                  {/* <p className="subtitle paragraphV3">
-                      {item?.educationCareer?.occupation
-                        ? item?.educationCareer?.occupation
+            {/* <p className="subtitle paragraphV3">
+                      {profile?.educationCareer?.occupation
+                        ? profile?.educationCareer?.occupation
                         : "Software Engineer"}
                     </p> */}
-                </div>
-                <div className="content">
-                  <div className="details">
-                    <p className="hide_desktop">#{item.userId}</p>
-                    <p className="subtitle paragraphV3">
-                      {item?.educationCareer?.occupation
-                        ? item?.educationCareer?.occupation
-                        : "Software Engineer"}
-                    </p>
-                    <p className="paragraphV3">
-                      {" "}
-                      {item?.dateOfBirth
-                        ? `${calculateAge(item?.dateOfBirth)} years`
-                        : "18 years"}
-                      ,{" "}
-                      {heightCalculator(item?.basicInfo?.height) ||
-                        `5 feet 10 inches`}
-                    </p>
-                    <p className="paragraphV3 text_bold">
-                      {`${item?.location.city ? item?.location.city : ""} ${item?.location.country
-                        }`}
-                    </p>
-                  </div>
-                </div>
-                <div className="px-20">
-                  <div className="border_line" />
-                </div>
-                <div className="footer  ">
-                  <div className="flex flex-gap-10 ">
-                    <div className="pointer circle_img">
-                      <img src="/dashboard/love.svg" alt="" />
-                    </div>
-                    <div className="pointer circle_img">
-                      <img src="/dashboard/chat.svg" alt="" />
-                    </div>
-                    <div className="pointer circle_img">
-                      <img src="/dashboard/star.svg" alt="" />
-                    </div>
-                  </div>
+          </div>
+          <div className="content">
+            <div className="details">
+              <p className="hide_desktop">#{profile?.userId}</p>
+              <p className="subtitle paragraphV3">
+                {profile?.educationCareer?.occupation
+                  ? profile?.educationCareer?.occupation
+                  : "Software Engineer"}
+              </p>
+              <p className="paragraphV3">
+                {" "}
+                {profile?.dateOfBirth
+                  ? `${calculateAge(profile?.dateOfBirth)} years`
+                  : "18 years"}
+                ,{" "}
+                {heightCalculator(profile?.basicInfo?.height) ||
+                  `5 feet 10 inches`}
+              </p>
+              <p className="paragraphV3 text_bold">
+                {`${profile?.location.city ? profile?.location.city : ""} ${
+                  profile?.location.country
+                }`}
+              </p>
+            </div>
+          </div>
+          <div className="px-20">
+            <div className="border_line" />
+          </div>
+          <div className="footer  ">
+            <div className="flex flex-gap-10 ">
+              <div className="pointer circle_img">
+                <img src="/dashboard/love.svg" alt="" />
+              </div>
+              <div className="pointer circle_img">
+                <img src="/dashboard/chat.svg" alt="" />
+              </div>
+              <div className="pointer circle_img">
+                <img src="/dashboard/star.svg" alt="" />
+              </div>
+            </div>
 
-                  <button
-                    className="custom-button custom_btn_size "
-                  // onClick={handleRedirect}
-                  >
-                    Send Request
-                  </button>
-                  {/* <Button
+            <button
+              className="custom-button custom_btn_size "
+              // onClick={handleRedirect}
+            >
+              Send Request
+            </button>
+            {/* <Button
                     variant="filled"
                     color="#9908F5"
                     size="md"
@@ -120,20 +121,20 @@ export const SearchCard = () => {
                   >
                     Button
                   </Button> */}
-                </div>
-                <div className="avatar">
-                  <img
-                    src={item?.profilePicture?.url?.medium || imageUrl}
-                    alt={item?.name}
-                  />
-                </div>
-              </div>
-            );
-          })}
+          </div>
+          <div className="avatar">
+            <img
+              src={profile?.profilePicture?.url?.medium || imageUrl}
+              alt={profile?.name}
+            />
+          </div>
         </div>
-      ) : (
+        {/* );
+          })} */}
+      </div>
+      {/* ) : (
         <>No data found</>
-      )}
+      )} */}
     </div>
   );
 };
